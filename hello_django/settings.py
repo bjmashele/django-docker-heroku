9549fd2c81cf7a859aa4bc56488b07b2e22285f3
bjmashele <bjmashele@gmail.com>
2019-07-04 14:02:13 -0500

    Added Postgres db

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url  # for PosgresQL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,6 +88,11 @@ DATABASES = {
     }
 }
 
+# add POSTGRES and default to SQLite
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
